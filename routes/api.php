3fa5714dd1139903be077948dc52cd1c16016fd0
Walid Wazini <wazini25@gmail.com>
2023-10-18 18:47:09 +0800

    Add request files (store n update) | add SurveyResource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SurveyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,16 +18,21 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/',function(){
     return response()->json(['message' => 'Welcome to Survey System API.']);
 });
 
 
-Route::middleware('auth:sanctum')->get('/auth/logout',[AuthController::class,'logout']);
+// Route::middleware('auth:sanctum')->get('/auth/logout',[AuthController::class,'logout']);
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::get('/auth/logout',[AuthController::class,'logout']);
+    Route::apiResource('survey',SurveyController::class);
+});
 
 Route::group(['prefix' => '/auth'], function() {
     Route::post('/signup', [AuthController::class,'signup']);
